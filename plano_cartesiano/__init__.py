29df91d97e88cf0fa59a1ff2688697c3e1939b10
Gramosa <63947567+Gramosa@users.noreply.github.com>
2020-04-19 04:27:35 -0300

    Add files via upload

---
+++ plano_cartesiano/__init__.py
@@ -0,0 +1,39 @@
+from functions import *
+from point import *
+
+def intersection_point(function1, function2):
+    """
+    this function returns a point from the intersection of two functions,
+    they need to have a different "a" value to touch
+    """
+    a = function1.get_a()
+    b = function1.get_b()
+    c = function2.get_a()
+    d = function2.get_b()
+    if a != c:#se "a" for igual a "c", as funções serão paralelas e nunca se tocarão
+        x = (d - b)/(a - c)
+        if x == int(x):
+            x = int(x)
+    else:
+        raise Exception('this functions will never intersect')
+    return function1.get_point(x)
+
+def generate_function(point1, point2):
+    """
+    This function return a Function objetc from the functions.py
+    this function is generated from the line connecting two points, these points cannot
+    have the same value of x
+    """
+    y = point1.get_y()
+    z = point2.get_y()
+    x = point1.get_x()
+    w = point2.get_x()
+    if x != w:#se "x" for igual a "w", esses pontos terão o mesmo valor de dominio. E não será possível determinar uma função entre eles
+        a = (y - z)/(x - w)
+        if z != y:#ja que os dominios são diferentes, se "z" for diferente de "y" descarta-se a possibilidade de função linear, e "b" precisa ser calculado
+            b = (a*(y*w - z*x))/(z - y)
+        else:#função linear
+            raise Exception('The function can\'t be constant, only "afim" functions') #poderia ser "y" ou "w", já que ambos são iguais. ou seja "b" será constante
+        return Function(a, b)
+    else:
+        raise Exception('domain of points can\'t be the same')
---
+++ plano_cartesiano/point.py
@@ -0,0 +1,87 @@
+def check_value(value):
+    """
+    Check if a value is numeric
+    """
+
+    try:
+        value / 2
+    except:
+        raise Exception('The value need be numeric')
+
+class Point():#Just a 2D point, A(x, y)
+    """
+    This class simulates 2D points, type A(x, y)
+
+    There are two atributes:
+    - x: is a float or integer number, this is the first coordinate for points
+    - y: is a float or integer number, this is the second coordinate for points
+    """
+    def __init__(self, pos_x, pos_y):
+        self.x = pos_x
+        self.y = pos_y
+
+    def get_x(self):
+        """
+        Return the x value
+        """
+        return self.x
+
+    def get_y(self):
+        """
+        Return the y value
+        """
+        return self.y
+
+    def set_x(self, new_x):
+        """
+        Set the value of x for a new numeric value
+        """
+        check_value(new_x)
+        self.x = new_x
+
+    def set_y(self, new_y):
+        """
+        Set the value of y for a new numeric value
+        """
+        check_value(new_y)
+        self.y = new_y
+
+    def is_in_func(self, function):
+        """
+        Parameters
+        __________
+        function: is an object from Function class in the archive functions.py
+        
+        Return True if the point is in function, a point is created using the self
+        value of x in the function, so if the y value of this new point is the same
+        of the self point. then these points are the same and return True. else return
+        False
+        """
+        tester = function.get_point(self.get_x())
+        if tester.get_y() == self.get_y():
+            return True
+        else:
+            return False
+
+    def get_location(self):
+        """
+        Only a visual way to see the coordinates
+        """
+        return f'x={self.get_x()}, y={self.get_y()}'
+
+    def get_location_array(self):
+        """
+        Get the coordinates in a list
+        """
+        loc = [self.get_x(), self.get_y()]
+        return loc
+
+def distance_points(point1, point2):
+    """
+    return a float number, that show the distance between two point in a cartesian plan
+    using the Pythagorean theorem
+    """
+    delta_x = abs(point1.get_x() - point2.get_x())
+    delta_y = abs(point1.get_y() - point2.get_y())
+    dist = (delta_x ** 2 + delta_y ** 2) ** (1/2)
+    return dist
